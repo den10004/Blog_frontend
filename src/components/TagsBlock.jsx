7b1c24b85ea9ea43714edf54e81da798d52a8d18
Denis Sergeev <denis-sergeev-85@inbox.ru>
2023-06-29 10:34:12 +0300

    сделана сортировка v 1.1.3

--- src/components/TagsBlock.jsx
+++ src/components/TagsBlock.jsx
@@ -7,6 +7,7 @@ import TagIcon from "@mui/icons-material/Tag";
 import ListItemText from "@mui/material/ListItemText";
 import Skeleton from "@mui/material/Skeleton";
 import "../index.scss";
+import "./TagsBlock.scss";
 
 import { SideBlock } from "./SideBlock";
 
